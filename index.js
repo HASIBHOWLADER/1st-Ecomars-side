7617fd94d091e4c292fd40285137e29e6e5de3ef
md hasib howlader <md.hasib.howlader.rony@gmail.com>
2022-07-13 14:04:08 +0600

    change feather

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.querySelector(".short").addEventListener("click",shortby);
 function shortby(){
    let degain= document.querySelector(".header2-bio");
-   degain.classList.add("active");
+   degain.classList.toggle("active");
 }
 document.querySelector(".great").addEventListener("click",removes)
 function removes(){
     let degain= document.querySelector(".header2-bio");
-    degain.classList.remove("active");
+    degain.classList.toggle("active");
 }
