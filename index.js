d0ee4006fce1007f0b07935b69c6d6dbc6c71ebf
md hasib howlader <md.hasib.howlader.rony@gmail.com>
2022-07-19 00:06:48 +0600

    feather change 4

--- index.js
+++ index.js
@@ -8,3 +8,41 @@ function removes(){
     let degain= document.querySelector(".header2-bio");
     degain.classList.toggle("active");
 }
+// time.........opan
+
+setInterval(()=>{
+    let date=new Date();
+    let hourse=date.getHours();
+    let min=date.getMinutes();
+    let sec=date.getSeconds();
+    if(hourse>12){
+hourse=hourse-12;
+    }if(min<10){
+min="0"+min;
+    }if(sec<10){
+sec="0"+sec;
+    }
+    let sum=hourse+ ":"+min+":"+sec;
+    console.log(sum);
+    document.querySelector(".time").innerHTML=sum;
+
+},100)
+
+setInterval(()=>{
+    let date=new Date();
+    let hourse=date.getHours();
+    let min=date.getMinutes();
+    let sec=date.getSeconds();
+    if(hourse>=12){
+hourse=hourse-12;
+    }if(min<10){
+min="0"+min;
+    }if(sec<10){
+sec="0"+sec;
+    }
+    let sum=hourse+ ":"+min+":"+sec;
+    console.log(sum);
+    document.querySelector(".times").innerHTML=sum;
+
+},100)
+// time close...........close
